c4984369cb0a03957763f13add2c33698c3db5cd
haetze <richy.sting@gmail.com>
2013-12-17 21:11:21 +0100

    import example

---
+++ ImportExample/goLib/forLoop.go
@@ -0,0 +1,9 @@
+package goLib
+
+import "fmt";
+
+func ForLoop(){
+	for i:=0; i<100; i++{
+		fmt.Println(i);
+	}
+}
---
+++ ImportExample/goLib/goLib.go
@@ -0,0 +1,11 @@
+package goLib
+
+import "fmt";
+
+func main(){
+	fmt.Println("test");
+}
+
+func T(){
+	fmt.Println("t");
+}
