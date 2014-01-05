ba16cecb47f1217e0fdd658e3f2b6312ad03a62a
haetze <richy.sting@gmail.com>
2014-01-05 20:16:43 +0100

    test

--- OS.go
+++ OS.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Print("Test of the runtime:\nGo 1.2 runs on ")
 	switch os := runtime.GOOS; os {
 		case "darwin":
-			fmt.Println("OS X.")
+			fmt.Println("OS X...")
 		case "linux":
 			fmt.Println("Linux.")
 		default:
