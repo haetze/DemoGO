0be54f9a22f3979254d6ea741a102bd89214b4c8
haetze <richy.sting@gmail.com>
2014-04-15 16:49:44 +0200

    Interface

--- interfaceExample.go
+++ interfaceExample.go
@@ -5,32 +5,29 @@ import "fmt"
 //MyString it can't because it doesn't the test() method  
 type Interface  interface{
 	test() string
-
-
 }
 
-type MyInt int
+type MyInt struct {
+	first int
+	second int
+}
+
 type MyString string
 
 func (d MyInt) test() string{
 	return "test"
 }
 
-func (d MyString) test() string{
-	return "test2"
-}
 
 func  run(d Interface) string{
 	return d.test()
 }
 
 func main(){
-	a := MyInt(9)
+	a := MyInt{9, 10}
 	b := MyString("rrrr")
 	c := Interface(a)
-	d := Interface(b)
 	fmt.Println(b)
 	fmt.Println(a)
 	fmt.Println(run(c))
-	fmt.Println(run(d))
 }
