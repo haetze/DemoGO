0acb1dd72844ee840277923585119948031d30be
haetze <richy.sting@gmail.com>
2014-05-18 10:22:44 +0200

    tcp

--- sort.go
+++ sort.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 	"sort"
+	"os"
+	"strconv"
 )
 func main (){
-	y := []int{12,43,1,6,333}
+	var y []int
+	if len(os.Args) == 1{
+		y = []int{12,43,1,6,333}
+	}else{
+		for _, m := range os.Args{
+			i, err := strconv.Atoi(m)
+			if err != nil {
+			}else{
+				y = append(y, i)
+			}
+		}
+	}
 	sort.Ints(y)
 	fmt.Println(y)
 }
