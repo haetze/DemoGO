1b6e300142c5a811eddf985a7efd552978aa8378
haetze <richy.sting@gmail.com>
2014-07-12 11:37:03 +0200

    -a

--- mailExample.go
+++ mailExample.go
@@ -11,12 +11,17 @@ func main() {
 		"richy.sting@gmail.com",
 		"V3J2@Quitlam",
 		"smtp.gmail.com")
+
+	fmt.Println("aut set up")
+
 	err := smtp.SendMail(
 		"smtp.gmail.com:583",
 		auth,
 		"richy.sting@gmail.com",
 		[]string{"richy.sting@googlemail.com"},
 		[]byte("test test"))
+
+	fmt.Println("send")
 
 	if err != nil {
 		fmt.Println(err)
