2ecd971474a6f52b372629ddbad4bced64fb179f
haetze <richy.sting@gmail.com>
2014-07-12 12:09:04 +0200

    -a

--- mailExample.go
+++ mailExample.go
@@ -3,28 +3,46 @@ package main
 import (
 	"fmt"
 	"net/smtp"
+	"strconv"
 )
 
+type User struct {
+	name     string
+	email    string
+	password string
+	server   string
+	port     int
+	auth     smtp.Auth
+}
+
 func main() {
-	auth := smtp.PlainAuth(
-		"",
-		"me@gmail.com",
-		"****************",
-		"smtp.gmail.com")
+	user := User{"", "me@gmail.com", "***********", "smtp.gmail.com", 587, nil}
+	user.auth = gmailAuth(user)
 
 	fmt.Println("aut set up")
 
-	err := smtp.SendMail(
-		"smtp.gmail.com:587",
-		auth,
-		"me@gmail.com",
-		[]string{"you@gmail.com"},
-		[]byte("test test"))
-
+	err := gmailSend(user, "you@gmail.com", "test test")
 	fmt.Println("send")
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
+}
+
+func gmailAuth(user User) smtp.Auth {
+	auth := smtp.PlainAuth(
+		user.name,
+		user.email,
+		user.password,
+		user.server)
+	return auth
+}
+
+func gmailSend(user User, to, body string) error {
+	err := smtp.SendMail(user.server+":"+strconv.Itoa(user.port), user.auth, user.email, []string{to}, []byte(body))
+	if err != nil {
+		return err
+	}
+	return nil
 }
