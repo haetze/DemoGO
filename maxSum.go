e4d7fb5cb9ad371a4d89782815e0371ccf66ee09
haetze <richy.sting@gmail.com>
2014-07-20 16:23:41 -0500

    -a

---
+++ maxSum.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sum := maxSum([]int{1, 3, 20, -9, 4, 3, 13, -9})
+	fmt.Println(sum)
+
+}
+
+func maxSum(x []int) []int {
+	if allSameSign(x) {
+		return sum(x)
+	} else if len(x) == 2 {
+	}
+
+	x = shrink(shrink(shrink(x)))
+	//fmt.Println(x)
+	n := findBig(x)
+	big := x[n]
+	x = genNew(x)
+	x = shrink(x)
+	if allSameSign(x) {
+		w := sum(x)
+		if w[0] < big {
+			return []int{big}
+		} else {
+			return w
+		}
+	} else {
+		b := findBig(x)
+		if x[b] < big {
+			return []int{big}
+		} else {
+			return []int{x[b]}
+		}
+	}
+
+	return x
+}
+
+func allSameSign(x []int) bool {
+	b := x[0] < 0
+	for n, i := range x {
+		if n == 0 {
+		} else {
+			if b != (i < 0) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func sum(x []int) []int {
+	var sum int
+	for _, n := range x {
+		sum = n + sum
+	}
+	return []int{sum}
+}
+
+func shrink(x []int) []int {
+	for n, i := range x {
+		if n < len(x)-1 {
+			if (i < 0 && x[n+1] < 0) || (i > 0 && x[n+1] > 0) {
+				x[n+1] = i + x[n+1]
+				x[n] = 0
+			}
+		}
+	}
+	x = dropZero(x)
+	return x
+}
+
+func dropZero(x []int) []int {
+	var b []int
+	for i := 0; i < len(x); i++ {
+		if x[i] != 0 {
+			b = append(b, x[i])
+		}
+	}
+	return b
+}
+
+func findBig(x []int) int {
+	n := 0
+	for i := range x {
+		if x[i] > x[n] {
+			n = i
+		}
+	}
+	return n
+}
+
+func genNew(x []int) []int {
+	if x[0] < 0 {
+		for i := range x {
+			if (i%2) == 1 && i < len(x)-2 {
+				if (x[i] * -1) < x[i+1] {
+					x[i+1] = x[i] + x[i+1]
+					x[i] = 0
+				}
+			}
+			if (i%2) == 1 && i != 0 {
+				if (x[i] * -1) < x[i-1] {
+					x[i+1] = x[i] + x[i-1]
+					x[i] = 0
+				}
+			}
+		}
+	}
+	if x[0] > 0 {
+		for i := range x {
+			if (i%2) == 0 && i < len(x)-2 {
+				if (x[i+1] * -1) < x[i] {
+					x[i] = x[i] + x[i+1]
+					x[i+1] = 0
+				}
+			}
+			if (i%2) == 0 && i != 0 {
+				if (x[i-1] * -1) < x[i] {
+					x[i] = x[i] + x[i-1]
+					x[i-1] = 0
+				}
+			}
+
+		}
+	}
+	x = dropZero(x)
+	return x
+}
