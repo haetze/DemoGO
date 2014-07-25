2c4ff59fad12ec1cb10dccd24e3fc1b9857e2a12
haetze <richy.sting@gmail.com>
2014-07-25 14:38:24 -0500

    tested

---
+++ TestList.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"GolangList"
+	"fmt"
+)
+
+func main() {
+	val1 := List.Element{List.Type{0, "1"}, nil, nil}
+	val2 := List.Element{List.Type{0, "1"}, nil, nil}
+	val3 := List.Element{List.Type{0, "1"}, nil, nil}
+	val4 := List.Element{List.Type{0, "1"}, nil, nil}
+	val5 := List.Element{List.Type{0, "1"}, nil, nil}
+	val6 := List.Element{List.Type{0, "1"}, nil, nil}
+	val7 := List.Element{List.Type{0, "1"}, nil, nil}
+	val8 := List.Element{List.Type{0, "1"}, nil, nil}
+	val9 := List.Element{List.Type{0, "1"}, nil, nil}
+	val0 := List.Element{List.Type{0, "1"}, nil, nil}
+	val11 := List.Element{List.Type{0, "1"}, nil, nil}
+	val12 := List.Element{List.Type{0, "1"}, nil, nil}
+	val13 := List.Element{List.Type{0, "1"}, nil, nil}
+	val14 := List.Element{List.Type{0, "1"}, nil, nil}
+	val15 := List.Element{List.Type{0, "1"}, nil, nil}
+	val16 := List.Element{List.Type{0, "1"}, nil, nil}
+	val17 := List.Element{List.Type{0, "1"}, nil, nil}
+
+	list := List.GenList([]List.Element{val1, val2, val3, val4, val5, val6, val7, val8, val9, val0, val11, val12, val13, val14, val15, val16, val17})
+	fmt.Println(list)
+
+	a, _ := list.Search("1211^")
+	fmt.Println(a)
+}
