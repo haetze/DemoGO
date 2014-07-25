ffb6d1a911143a7900d3154b69cc62c698df5015
haetze <richy.sting@gmail.com>
2014-07-25 15:01:39 -0500

    tested2

--- TestList.go
+++ TestList.go
@@ -5,24 +5,36 @@ import (
 	"fmt"
 )
 
+type Type struct {
+	key int
+	val string
+}
+
+func (t Type) Key() int {
+	return t.key
+}
+func (t Type) Val() string {
+	return t.val
+}
+
 func main() {
-	val1 := List.Element{List.Type{0, "1"}, nil, nil}
-	val2 := List.Element{List.Type{0, "1"}, nil, nil}
-	val3 := List.Element{List.Type{0, "1"}, nil, nil}
-	val4 := List.Element{List.Type{0, "1"}, nil, nil}
-	val5 := List.Element{List.Type{0, "1"}, nil, nil}
-	val6 := List.Element{List.Type{0, "1"}, nil, nil}
-	val7 := List.Element{List.Type{0, "1"}, nil, nil}
-	val8 := List.Element{List.Type{0, "1"}, nil, nil}
-	val9 := List.Element{List.Type{0, "1"}, nil, nil}
-	val0 := List.Element{List.Type{0, "1"}, nil, nil}
-	val11 := List.Element{List.Type{0, "1"}, nil, nil}
-	val12 := List.Element{List.Type{0, "1"}, nil, nil}
-	val13 := List.Element{List.Type{0, "1"}, nil, nil}
-	val14 := List.Element{List.Type{0, "1"}, nil, nil}
-	val15 := List.Element{List.Type{0, "1"}, nil, nil}
-	val16 := List.Element{List.Type{0, "1"}, nil, nil}
-	val17 := List.Element{List.Type{0, "1"}, nil, nil}
+	val1 := List.Element{Type{0, "1"}, nil, nil}
+	val2 := List.Element{Type{0, "1"}, nil, nil}
+	val3 := List.Element{Type{0, "1"}, nil, nil}
+	val4 := List.Element{Type{0, "1"}, nil, nil}
+	val5 := List.Element{Type{0, "1"}, nil, nil}
+	val6 := List.Element{Type{0, "1"}, nil, nil}
+	val7 := List.Element{Type{0, "1"}, nil, nil}
+	val8 := List.Element{Type{0, "1"}, nil, nil}
+	val9 := List.Element{Type{0, "1"}, nil, nil}
+	val0 := List.Element{Type{0, "1"}, nil, nil}
+	val11 := List.Element{Type{0, "1"}, nil, nil}
+	val12 := List.Element{Type{0, "1"}, nil, nil}
+	val13 := List.Element{Type{0, "1"}, nil, nil}
+	val14 := List.Element{Type{0, "1"}, nil, nil}
+	val15 := List.Element{Type{0, "1"}, nil, nil}
+	val16 := List.Element{Type{0, "1"}, nil, nil}
+	val17 := List.Element{Type{0, "1"}, nil, nil}
 
 	list := List.GenList([]List.Element{val1, val2, val3, val4, val5, val6, val7, val8, val9, val0, val11, val12, val13, val14, val15, val16, val17})
 	fmt.Println(list)
