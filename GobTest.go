fc9991932c93edc63376afec44cc3c6c8bf412b9
haetze <richy.sting@gmail.com>
2014-08-17 08:08:06 -0500

    persistent user data in gob format

---
+++ GobTest.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+)
+
+type Q struct {
+	N int
+	B int
+}
+
+func main() {
+	var pipe bytes.Buffer
+	/*	enc := gob.NewEncoder(&pipe)
+		err := enc.Encode(Q{12, 12})
+		e := ioutil.WriteFile("gobEx", pipe.Bytes(), 0644)
+		fmt.Println(e)*/
+	var s Q
+	b, _ := ioutil.ReadFile("gobEx")
+	pipe = *bytes.NewBuffer(b)
+	fmt.Println(pipe)
+	dec := gob.NewDecoder(&pipe)
+	err2 := dec.Decode(&s)
+
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	fmt.Println(s)
+
+}
