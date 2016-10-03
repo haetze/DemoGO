46c534a118b41a634dd4e618020a0c2c088de940
Richard Stewing <richy.sting@gmail.com>
2016-10-03 15:14:21 -0700

    bin tree

--- binTree.go
+++ binTree.go
@@ -7,21 +7,57 @@ import (
 
 type Node struct{
 	val int
-	node1 *Node
-	node2 *Node
+	left *Node
+	right *Node
+}
+
+func (n *Node) inOrderPrint(){
+	if n.left != nil {
+		n.left.inOrderPrint();
+	}
+	fmt.Println(n.val);
+	if n.right != nil {
+		n.right.inOrderPrint();
+	}
 }
 
 func (n *Node) init(val int){
 	n.val = val
 }
 
+func (n *Node) add(m *Node){
+	if n.val > m.val {
+		if n.left == nil {
+			n.left = m
+		}else{
+			n.left.add(m)
+		}
+	}else if n.val < m.val {
+		if n.right == nil {
+			n.right = m
+		}else{
+			n.right.add(m)
+		}
+	}
+}
+
+
 func main(){
-	node := Node{}
-	node.init(12)
-	node2 := Node{}
-	node2.init(32)
-	node.node2 = &node2
-	fmt.Println(*node.node2)
-	node.node2.val = 34
-	fmt.Println(node2)
+	n := Node{}
+	n.init(12)
+	n1 := Node{}
+	n1.init(6)
+	n2 := Node{}
+	n2.init(24)
+	n3 := Node{}
+	n3.init(16)
+	n4 := Node{}
+	n4.init(9)
+	n.add(&n1)
+	n.add(&n2)
+	n.add(&n3)
+	n.add(&n4)
+	n.inOrderPrint()
+	//fmt.Println("Hello World!")
+	
 }
